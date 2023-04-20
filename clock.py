0ff5c110c3d30f901cc50797cb3f067ceb7de1ef
Felipe Pereira <70305395+Flepsz@users.noreply.github.com>
2023-04-20 10:18:38 -0300

    Delete clock.py

--- clock.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def get_curr_time():
-    # datetime object containing current date and time
-    now = datetime.now()
-    # dd/mm/YY H:M:S
-    current_timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
-    return current_timestamp
