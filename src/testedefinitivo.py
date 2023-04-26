35c1b8e66a7a14589420558accbbab9d34ac789c
Felipe Pereira <70305395+Flepsz@users.noreply.github.com>
2023-04-26 11:00:18 -0300

    Update testedefinitivo.py

--- src/testedefinitivo.py
+++ src/testedefinitivo.py
@@ -6,13 +6,13 @@ from RPLCD import CharLCD
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 import connect as con
+import threading
 
 leitorRfid = SimpleMFRC522()
 
 # Raspberry Pi pin setup
 lcd = CharLCD(numbering_mode=GPIO.BOARD, cols=16, rows=2, pin_rs=37, pin_e=18, pins_data=[16, 11, 12, 15])
 lcd.clear()
-
 
 sql = "SELECT rfID FROM students"
 con.cursor.execute(sql)
@@ -24,29 +24,38 @@ for i in result:
 
 print("Aproxime o cartao da leitora...")
 
-while True:
-    lcd.write_string(datetime.now().strftime('%b %d  %H:%M:%S\n'))
-    sleep(0.95)
+def clock_thread():
+    while True:
+        lcd.cursor_pos = (0, 0)
+        lcd.write_string(datetime.now().strftime('%b %d  %H:%M:%S\n'))
+        sleep(0.95)
 
+clock = threading.Thread(target=clock_thread)
+clock.start()
+
+while True:
     try:
-        
         id = leitorRfid.read_id()
         print("ID do aluno: ", id)
         lcd.clear()
-
+        lcd.cursor_pos = (1, 0)
         if str(id) in list_id:
-            print(id)
             sql = f"SELECT nameStudent FROM students WHERE rfID = '{id}'"
             con.cursor.execute(sql)
             result = con.cursor.fetchone()
 
             hit_point(id)
-            lcd.write_string(f"{result[0]} registrou")
-            time.sleep(5)
+            lcd.write_string(f"{result[0]}")
+            time.sleep(2)
+            lcd.clear()
+            lcd.cursor_pos = (1, 0)
+            lcd.write_string("Registro aceito")
+            time.sleep(2)
             lcd.clear()
         else:
-            lcd.write_string("Tag RFID nao permitida!")
-            time.sleep(5)
-            
-    except:
+            lcd.write_string("Tag Invalida!")
+            time.sleep(2)
+            lcd.clear()
+    except Exception as e:
+        print("Erro: ", e)
         sleep(1)
